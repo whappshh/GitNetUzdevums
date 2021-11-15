35621e511afd54a524a1fa1a54c88de32a48c922
cpplv <aliepins@inbox.lv>
2021-11-15 20:55:32 +0200

    Add project files.

---
+++ GitNetUzdevums/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication_15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(594, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 368);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "y=sinx";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "y=x*x";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 53);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Andrejs Liepiņš";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Funkciju grafiki";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ GitNetUzdevums/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double m = 30; //mērogs
+            int xc = pictureBox1.Width / 2;   // centra koordinātes
+            int yc = pictureBox1.Height / 2;
+            int xe, ye;     // punktu "ekrāna" koordinātes
+            double x, y;   // punktu "matemātiskās" koordinātes
+            double step = 0.005;    // solis
+            Graphics G = pictureBox1.CreateGraphics();
+            G.Clear(System.Drawing.Color.White);
+            Pen myPen = new Pen(Color.Silver);
+            G.DrawLine(myPen, 10, yc, 2 * xc - 10, yc);   // asis
+            G.DrawLine(myPen, xc, 10, xc, 2 * yc - 10);
+            myPen = new Pen(Color.Black);
+            x = -Math.PI;
+            // funkcijas grafika konstruēšanas cikls
+            while (x < Math.PI)
+            {
+                try   // ja funkcija kādā punktā neeksistē 
+                {
+                    y = 1/Math.Sin(x);   // Funkcijas formula!!!
+                    xe = (int)(xc + m * x);
+                    ye = (int)(yc - m * y);
+                    G.DrawEllipse(myPen, xe, ye, 1, 1);
+                }
+                catch { }
+                x += step;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double m = 30; //mērogs
+            int xc = pictureBox1.Width / 2;   // centra koordinātes
+            int yc = pictureBox1.Height / 2;
+            int xe, ye;     // punktu "ekrāna" koordinātes
+            double x, y;   // punktu "matemātiskās" koordinātes
+            double step = 0.005;    // solis
+            Graphics G = pictureBox1.CreateGraphics();
+            G.Clear(System.Drawing.Color.White);
+            Pen myPen = new Pen(Color.Silver);
+            G.DrawLine(myPen, 10, yc, 2 * xc - 10, yc);   // asis
+            G.DrawLine(myPen, xc, 10, xc, 2 * yc - 10);
+            myPen = new Pen(Color.Black);
+            x = -Math.PI;
+            // funkcijas grafika konstruēšanas cikls
+            while (x < Math.PI)
+            {
+                try   // ja funkcija kādā punktā neeksistē 
+                {
+                    y = x * x; // Funkcijas formula!!!
+                    xe = (int)(xc + m * x);
+                    ye = (int)(yc - m * y);
+                    G.DrawEllipse(myPen, xe, ye, 1, 1);
+                }
+                catch { }
+                x += step;
+            }
+        }
+    }
+}
